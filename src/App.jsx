e851dc0281a954e4d934edd3c4897cf5256097dd
hanishbansal231 <hanishbansal803@gmail.com>
2023-09-28 16:25:49 +0530

    configures auto import sorting for eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css'
+import './App.css';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    lms
+    <Routes>
+      <Route />
+    </Routes>
     </>
   )
 }
