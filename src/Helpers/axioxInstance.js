4ef38cb85872e9bad9db524e5e0bc2d3a6fb1597
hanishbansal231 <hanishbansal803@gmail.com>
2023-09-28 16:46:54 +0530

    setup of axios instance

---
+++ src/Helpers/axioxInstance.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASE_URL = process.env.BASE_URL;
+
+const axiosInstance = axios.create();
+
+
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = true;
+
+export default axiosInstance;
